f50d1bb505682cccb097c28f17513e4fecd93d13
Andrii Hut <swear.walker.sw@gmail.com>
2025-05-09 23:47:50 +0300

    Release/v0.0.2 (#3)
    * feat: add Tailwind CSS and UI components
    
    - Updated package.json to include Tailwind CSS, Reka UI, and related dependencies.
    - Configured TypeScript paths and types in tsconfig.json.
    - Created a new .env.example file for environment variables.
    - Added tailwind.css for styling with Tailwind CSS and custom variables.
    - Introduced components.json for Shadcn Vue configuration.
    - Implemented a Button component with variants and styles.
    - Added i18n support with English and Ukrainian translations.
    - Created layouts for authentication and default views.
    - Developed pages for AI Chat, Sign In, Sign Up, Profile, Stripe, and TODO List.
    - Enhanced the index page with a welcome button and alert functionality.
    
    * chore: bump version to 0.0.2 in package.json

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <main class="main">
+        <h1>{{ $t('title') }}</h1>
+        <Button @click="showWelcomeAlert">Welcome button</Button>
+    </main>
+</template>
+
+<script setup lang="ts">
+import { Button } from '~/components/ui/button';
+
+const showWelcomeAlert = () => {
+    alert('Welcome to the Apps!');
+};
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="main">
+        <div class="flex items-center justify-center min-h-screen w-full">
+            <h1>Profile</h1>
+        </div>
+    </main>
+</template>
