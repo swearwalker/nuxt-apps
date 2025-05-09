a343a4c59090766d309f5f50df4437a16095bf18
swearwalker <swear.walker.sw@gmail.com>
2025-05-09 23:42:29 +0300

    feat: add Tailwind CSS and UI components
    - Updated package.json to include Tailwind CSS, Reka UI, and related dependencies.
    - Configured TypeScript paths and types in tsconfig.json.
    - Created a new .env.example file for environment variables.
    - Added tailwind.css for styling with Tailwind CSS and custom variables.
    - Introduced components.json for Shadcn Vue configuration.
    - Implemented a Button component with variants and styles.
    - Added i18n support with English and Ukrainian translations.
    - Created layouts for authentication and default views.
    - Developed pages for AI Chat, Sign In, Sign Up, Profile, Stripe, and TODO List.
    - Enhanced the index page with a welcome button and alert functionality.

---
+++ pages/auth/sign-up.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="main">
+        <div class="flex items-center justify-center min-h-screen w-full">
+            <h1>SIgn Up</h1>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'auth',
+})
+</script>
---
+++ pages/stripe.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="main">
+        <div class="flex items-center justify-center min-h-screen w-full">
+            <h1>Stripe</h1>
+        </div>
+    </main>
+</template>
